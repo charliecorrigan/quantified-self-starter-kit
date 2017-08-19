4581dce5aa57faa9c2860e9c839e1130767fcbb7
Aaron Olsen <mraaronolsen@gmail.com>
2017-08-18 22:56:30 -0600

    meals

--- test/unit/meal-test.js
+++ test/unit/meal-test.js
@@ -1,29 +1,31 @@
 const assert = require('chai').assert;
 const pry = require('pryjs');
-const Food = require('../../lib/javascript/meal');
+const Food = require('../../lib/javascript/food');
 const Meal = require('../../lib/javascript/meal');
 
 describe('Meal', function(){
 
   beforeEach(function(){
-    meal = new Meal({name: 'Breakfast'})
+    meal = new Meal({name: 'Breakfast', id: 1})
     food = new Food({name: 'Spam', calories: 1200, id: 1})
   })
 
   it('is Meal', function(){
-    assert.equal(meal.constructor.name, 'Food')
+    assert.equal(meal.constructor.name, 'Meal')
   });
 
   it('has attributes', function(){
     assert.equal(meal.name, 'Breakfast');
-    assert.equal(meal.foods, []);
+    assert.equal(meal.id, 1);
+    assert.equal(meal.base, 0);
+    assert.deepEqual(meal.foods, []);
   });
 
   it('can add and remove a food', function(){
     meal.addFood(food);
-    assert.deep_equal(meal.foods, [food]);
+    assert.deepEqual(meal.foods, [food]);
 
     meal.removeFood(food);
-    assert.deep_equal(meal.foods, []);
+    assert.deepEqual(meal.foods, []);
   });
 });
