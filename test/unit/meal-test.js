66ea7fba03dbd4cceae629512b4ad1047eccd508
Aaron Olsen <mraaronolsen@gmail.com>
2017-08-18 23:05:31 -0600

    Meal Calculations

--- test/unit/meal-test.js
+++ test/unit/meal-test.js
@@ -28,4 +28,32 @@ describe('Meal', function(){
     meal.removeFood(food);
     assert.deepEqual(meal.foods, []);
   });
+
+  it('can have a base', function(){
+    meal.addFood(food);
+    meal.base = 2000;
+
+    assert.equal(meal.base, 2000);
+  });
+
+  it('can calculate calories', function(){
+    food1 = new Food({name: 'Spam', calories: 1200, id: 1});
+    food2 = new Food({name: 'Mutton', calories: 800, id: 2});
+
+    meal.addFood(food1);
+    meal.addFood(food2);
+
+    assert.equal(meal.total(), 2000);
+  });
+
+  it('can calculate remaining calories', function(){
+    food1 = new Food({name: 'Spam', calories: 1200, id: 1});
+    food2 = new Food({name: 'Mutton', calories: 800, id: 2});
+
+    meal.addFood(food1);
+    meal.addFood(food2);
+    meal.base = 2200;
+
+    assert.equal(meal.remaining(), 200);
+  })
 });
